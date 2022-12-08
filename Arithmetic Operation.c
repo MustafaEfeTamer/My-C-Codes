e88b859fa2dbaf7fac3de938f56093b4ba3ce4f1
MustafaEfeTamer <119308432+MustafaEfeTamer@users.noreply.github.com>
2022-12-08 16:41:27 +0300

    Create  Arithmetic Operation.c

---
+++ Arithmetic Operation.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+//arithmetic operation
+{
+    int a,b,c,d;
+    printf("please enter 4 number\n");
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    printf("ad^2+bd+c conclusion=%d",a*d*d+b*d+c);
+    return 0;
+
+}
+
