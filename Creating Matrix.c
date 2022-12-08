56b866d042066195c821b7696f5605ba6f8a60fa
MustafaEfeTamer <119308432+MustafaEfeTamer@users.noreply.github.com>
2022-12-08 16:44:20 +0300

    Update Creating Matrix.c

--- Creating Matrix.c
+++ Creating Matrix.c
@@ -5,7 +5,7 @@ int main()
     int efe[2][3][2];
     int i,j,k;
 
-    printf("12 adet sayi gir\n");
+    printf("please enter 12 number\n");
 
     for(i=0; i<2; i++)
     {
@@ -17,7 +17,7 @@ int main()
             }
         }
       }
-    printf("girdiginiz degerler");
+    printf("entered numbers");
     for(i=0;i<2;i++)
        for(j=0; j<3;j++)
         {
